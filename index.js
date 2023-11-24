bd6b891fef67a1a545d139aa484e727b666aa2f8
lasha <lasha.katchkatchishvili.1@btu.edu.ge>
2023-11-24 21:15:04 +0400

    commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// Write a recursive function that prints numbers from a given positive integer down to 1.
+
+
+
+// function printNumber(num){
+//     if(num==0){
+//         console.log("amoiwura ricxvebi");
+//         return;
+//     }else{
+//         console.log(num);
+//         return printNumber(num-1)
+//     }
+
+// }
+// printNumber(7)
+
+
+// 2 Create a recursive function to find the length of a given string.
+
+// function findLength(str,count=0){
+// if(str==""){
+//     return count;
+// }else{
+//    return findLength(str.slice(0,-1),count+1)
+// }
+// }
+// console.log(findLength('hello world'));
+
+// Write a recursive function to determine if a given integer is even or odd.
+
+
+// function even_or_odd(num){
+// if(num==0){
+//     return "luwia";
+// }
+// if(num==1){
+//     return "kentia";
+// }
+
+// return even_or_odd(num-2);
+
+
+// }
+
+
+// console.log(even_or_odd(2));
+
+// Write a recursive function to find the sum of the first n natural numbers.
+
+// function sum(number){
+
+//     if(number<=1){
+//         return number;
+//     }else{
+//         return number +sum(number-1);
+//     }
+
+
+// }
+// console.log(sum(8));
+
